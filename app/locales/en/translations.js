e3d4bf253163b0cd6e5a02712fde58840b9f73b2
ronco <ronco@costite.com>
2015-09-19 22:29:30 -0600

    about route

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -2,5 +2,10 @@ export default {
   'index': {
     'headline': 'Frozen Bananas!',
     'subTitle': 'Coming Soon'
+  },
+  'about': {
+    'title': 'Bluth Banana Stand',
+    'number': 'In a van',
+    'street': 'Down by the river'
   }
 };
