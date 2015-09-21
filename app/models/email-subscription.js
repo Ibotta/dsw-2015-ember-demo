79981afa83ae49ab3e5aecaf6a27e3b4a50c4b72
ronco <ronco@costite.com>
2015-09-21 10:47:29 -0600

    validations with ember-cp-validations

--- app/models/email-subscription.js
+++ app/models/email-subscription.js
@@ -1,6 +1,17 @@
 import DS from 'ember-data';
+import {
+  validator, buildValidations
+}
+from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+  email: [
+    validator('presence', true),
+    validator('format', { type: 'email' })
+  ]
+});
+
+export default DS.Model.extend(Validations, {
   email:     DS.attr('string'),
   marketing: DS.attr('boolean', {
     defaultValue: true
