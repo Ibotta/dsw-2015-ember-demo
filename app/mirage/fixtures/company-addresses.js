7000fe0890aaed80e326051cb562491cbc5a4951
ronco <ronco@costite.com>
2015-09-20 20:36:21 -0600

    ember-cli-mirage for company-addresses

---
+++ app/mirage/fixtures/company-addresses.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    id: 1,
+    name: 'Bluth\'s Banana Stand',
+    street1: 'In a Van',
+    street2: 'Down by the river',
+    city: 'Denver',
+    state: 'CO',
+    zip: 80202
+  }
+];
